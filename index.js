5327421c2c0c034f4ad7deb5556ba79a0b84fdf1
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-03-26 17:32:35 -0600

    Pokedex, new routes and RegEx

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { pokemon } = require("./pokedex.json");
 
 /*
 Verbos HTTP
@@ -12,8 +13,40 @@ DELETE
 
 app.get("/", (req, res, next)=>{
     res.status(200);
-    res.send("Hola mundo");
-})
-app.listen(3000, ()=>{
+    res.send("Bienvenido al Pokedex");
+});
+
+app.get("/pokemon/all", (req, res, next)=>{
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get("/pokemon/:id([0-9]{1,3})", (req, res, next)=>{
+    
+    const id = req.params.id - 1;
+
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id - 1])
+    }else{
+        res.status(404);
+        res.send("Pokemon no encontrado")
+    }
+});
+
+app.get("/pokemon/:name" , (req, res, next)=>{
+    const name = req.params.name;
+
+    for(let i=0; i<pokemon.length; i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...");
-})
+});
