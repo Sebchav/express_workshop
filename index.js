a0c46267f03823887c88938b563f190bb94a88ba
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-17 20:37:43 -0600

    Middleware para 404 y cambiando el body-parser

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ DELETE
 */
 
 app.get("/", (req, res, next)=>{
-    return res.status(200).send("Bienvenido al Pokedex");
+    return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"});
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next)=>{
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...");
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,7 +8,7 @@ pokemon.post("/", (req, res, next)=>{
 
 pokemon.get("/", async(req, res, next)=>{
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 });
 
 pokemon.get("/:id([0-9]{1,3})", async(req, res, next)=>{
@@ -16,9 +16,9 @@ pokemon.get("/:id([0-9]{1,3})", async(req, res, next)=>{
     const id = req.params.id;
     const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id}`);
 
-    (id >= 0 && id <= 722) ? 
-        res.status(200).json(pkmn) : 
-        res.status(404).send("Pokemon no encontrado");
+    (id >= 1 && id <= 722) ? 
+        res.status(200).json({code: 1, message: pkmn}) : 
+        res.status(404).send({code: 404, message: "Pokemon no encontrado"});
    
 });
 
@@ -28,8 +28,8 @@ pokemon.get("/:name([A-Za-z]+)" , async(req, res, next)=>{
     const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_name = '${name.toLowerCase()}'`);
 
     (pkmn.length > 0) ?
-         res.status(200).json(pkmn) :
-         res.status(404).send("Pokemon no encontrado");
+        res.status(200).json({code: 1, message: pkmn}) : 
+        res.status(404).send({code: 404, message: "Pokemon no encontrado"});
 });
 
 module.exports = pokemon;
